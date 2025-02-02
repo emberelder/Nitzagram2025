699e9a86dd778c1a547092bb6ab22880724b0f68
sofiyaoleynik <sofiyaoleynik20082008@gmail.com>
2025-02-02 17:47:59 +0200

    POSLANIYE POTOMKAM

--- classes/Post.py
+++ classes/Post.py
@@ -5,31 +5,15 @@ from helpers import screen
 
 
 class Post:
-    """
-    A class used to represent post on Nitzagram
-    """
-    def __init__(self): #TODO: add parameters
-        #TODO: write me!
-        pass
-
-    def display(self):
-        """
-        Display the Post image/Text, description, location, likes and comments
-        on screen
-
-        :return: None
-        """
-        # TODO: write me!
-        pass
+    def __init__(self, username, location, description):
+        self.username = username
+        self.location = location
+        self.description = description
+        self.likes_counter = 0
+        self.comments = []
 
 
     def display_comments(self):
-        """
-        Display comments on post. In case there are more than 4
-        comments, show only 4 comments chosen by reset_comments_display_index
-
-        :return: None
-        """
         position_index = self.comments_display_index
         # If there are more than 4 comments, print "view more comments"
         if len(self.comments) > NUM_OF_COMMENTS_TO_DISPLAY:
@@ -39,7 +23,6 @@ class Post:
                                                             True, LIGHT_GRAY)
             screen.blit(view_more_comments_button, (VIEW_MORE_COMMENTS_X_POS,
                                                     VIEW_MORE_COMMENTS_Y_POS))
-
         # Display 4 comments starting from comments_display_index
         for i in range(0, len(self.comments)):
             if position_index >= len(self.comments):
@@ -49,5 +32,30 @@ class Post:
             if i >= NUM_OF_COMMENTS_TO_DISPLAY - 1:
                 break
 
+    def display_username(self):
+        font = pygame.font.SysFont("chalkduster.ttf", 18)
+        un = font.render(self.username, True, BLACK)
+        screen.blit(un, [USER_NAME_X_POS, USER_NAME_Y_POS])
 
+    def display_location(self):
+        font = pygame.font.SysFont("chalkduster.ttf", 16)
+        loc = font.render(self.location, True, BLACK)
+        screen.blit(loc, [LOCATION_TEXT_X_POS, LIKE_BUTTON_Y_POS])
 
+    def display_description(self):
+        font = pygame.font.SysFont("chalkduster.ttf", 14)
+        des = font.render(self.description, True, BLACK)
+        screen.blit(des, [DESCRIPTION_TEXT_X_POS, DESCRIPTION_TEXT_Y_POS])
+
+    def display_likes(self):
+        font = pygame.font.SysFont("chalkduster.ttf", 14)
+        likes = font.render(self.likes_counter, True, BLACK)
+        screen.blit(likes, [LIKE_TEXT_X_POS, LIKE_TEXT_Y_POS])
+
+    def display(self):
+        self.display_likes()
+        self.display_description()
+        self.display_location()
+        self.display_username()
+        self.display_comments()
+        pass
